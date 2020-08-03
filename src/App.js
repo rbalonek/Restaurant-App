f660248142367b5db0b89a243081f38267237dcd
Robert Balonek <robertbalonek@Roberts-MacBook.local>
2020-08-03 09:37:07 -0400

    Confirmed API functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+// import { Route } from "react-router-dom";
+import axios from "axios";
 import './App.css';
 
+const BASE_URL =
+  "https://api.airtable.com/v0/app9S6k06MQoTSJbG/Menu/recAxDasgD2mOAMkV";
+
 function App() {
+  const [mains, updateMains] = useState([]); // to store our records
+  // const [fetchEntries, invokeFetch] = useState(true); // grab new records
+  // const [thing, updateThing] = useEffect([]);
+
+  useEffect(() => {
+    const getAirtableRecords = async () => {
+      const data = await axios.get(`${BASE_URL}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      updateMains(data.data);
+      console.log(data);
+    };
+    getAirtableRecords();
+  }, []);
+
   return (
     <div>
       <h1>Hi</h1>
