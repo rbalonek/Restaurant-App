f7093fb41b1628d4112088dd0c61b1d53dfc16a3
Robert Balonek <robertbalonek@Roberts-MacBook.local>
2020-08-04 11:38:58 -0400

    Basic Cards formed

--- src/live_menu/Guest_Components/GuestMenuItem.js
+++ src/live_menu/Guest_Components/GuestMenuItem.js
@@ -1,17 +1,58 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginLeft: 50,
+    // marginBottom: 30,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 export default function MenuItem(props) {
-  const { item, type } = props
+  const classes = useStyles();
+  const { item, type } = props;
+
+
   return (
-    // <Link to={`/EditMenu/${type}/${item.id}`}>
-      <p key={item.fields.name}>
-      {item.fields.name}
-      <br></br>
-      {item.fields.notes}
-      <br></br>
-      {item.fields.price}
-    </p>
-    // </Link>
+   
+    
+    <Card className={classes.root}>
+    <CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+        {item.fields.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+        {item.fields.price}, {item.fields.notes}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Add to Order
+      </Button>
+    </CardActions>
+  </Card>
+
+    
   );
 }
--- src/live_menu/LiveMenu.js
+++ src/live_menu/LiveMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GuestNavBar from "../components/Guest_Components/GuestNavBar"
+import "./LiveMenu.css"
 
 /// Menu Components
 import GuestMenuItem from "./Guest_Components/GuestMenuItem"
@@ -13,16 +14,16 @@ export default function LiveMenu(props) {
         <GuestNavBar />
       </header>
 
-      <h1>Apps</h1>
+      <h1 className="menu-titles">Apps</h1>
       {props.apps.map((app) => <GuestMenuItem key={app.id} item={app} type="app" />)}
       
-      <h1>Mains</h1>
+      <h1 className="menu-titles">Mains</h1>
       {props.mains.map((main) => <GuestMenuItem key={main.id} item={main} type="main" />)}
       
-      <h1>Drinks</h1>
+      <h1 className="menu-titles">Drinks</h1>
       {props.drinks.map((drink) => <GuestDrinkItem key={drink.id} item={drink} type="drink" />)}
     
-      <h1>Wine / Cocktails</h1>
+      <h1 className="menu-titles">Wine / Cocktails</h1>
       {props.alcoholDrinks.map((alcoholDrink) => <GuestDrinkItem key={alcoholDrink.id} item={alcoholDrink} type="alcoholDrink" />)}
     </div>
   )
