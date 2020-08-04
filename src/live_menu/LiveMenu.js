0dac1dcbe27ba4696c3769471b08d0f81d2f5e9e
Robert Balonek <robertbalonek@Roberts-MacBook.local>
2020-08-04 17:55:29 -0400

    Cleaning up

--- src/live_menu/LiveMenu.js
+++ src/live_menu/LiveMenu.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import GuestNavBar from "../components/Guest_Components/GuestNavBar"
 import "./LiveMenu.css"
 
 /// Menu Components
 import GuestMenuItem from "./Guest_Components/GuestMenuItem"
 import GuestDrinkItem from "./Guest_Components/GuestDrinkItem"
-// import TestCard from "./Guest_Components/TestCard"
 import Nav from "./Nav"
 
 
