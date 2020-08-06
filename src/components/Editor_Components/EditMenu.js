4274b8b9f07d1560735e50f4f44d519a5cfe5b69
Robert Balonek <robertbalonek@Roberts-MacBook.local>
2020-08-06 14:50:59 -0400

    EditMenu looks better

--- src/components/Editor_Components/EditMenu.js
+++ src/components/Editor_Components/EditMenu.js
@@ -5,11 +5,41 @@ import "./Create_Menu_Item/CreateMenuItem.css"
 //components
 import DeleteButton from "./DeleteButton"
 
+/// For Card
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+
+    
+    // marginLeft: 50,
+    marginBottom: 50,
+  },
+  media: {
+    height: 100,
+    minWidth: 200,
+    // maxHeight: 100,
+    // borderRadius: 80,
+  },
+  items: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 
 
 export default function EditMenu(props) {
+  const classes = useStyles();
   // console.log(props.apps)
   const [name, updateName] = useState('');
   const [notes, updateNotes] = useState('');
@@ -57,14 +87,33 @@ export default function EditMenu(props) {
      </Link>
      <Link to="/CreateMenuItem">
      <button className="create-menu-item-button">Create Menu Item</button>
-     </Link>
+      </Link>
+      
+
+<div className={classes.items}>
+<Card className={classes.root}>
+  <CardActionArea>
+    <CardMedia
+      className={classes.media}
+      image={imglink}
+      title={name}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+        {name}
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+        {price}, {notes}
+      </Typography>
+    </CardContent>
+  </CardActionArea>
+</Card>
+</div>
+
+
      
       <form onSubmit={handleSubmit}>
-        <h2>Edit Menu</h2>
-        <img src={imglink} width="200" height="200" alt={imglink}  />
-        <h1>{name}<br />{notes}<br />{price}<br /></h1>
-        
-        
+       
         <label htmlFor="name">Name</label>
         <input type="Text" id="name" onChange={e => updateName(e.target.value)} value={name} />
         
@@ -89,3 +138,28 @@ export default function EditMenu(props) {
     </div>
   );
 }
+
+
+// <div className={classes.items}>
+// <Card className={classes.root}>
+//   <CardActionArea>
+//     <CardMedia
+//       className={classes.media}
+//       image={item.fields.imglink}
+//       title={item.fields.name}
+//     />
+//     <CardContent>
+//       <Typography gutterBottom variant="h5" component="h2">
+//         {item.fields.name}
+//       </Typography>
+//       <Typography variant="body2" color="textSecondary" component="p">
+//         {item.fields.price}, {item.fields.notes}
+//       </Typography>
+//     </CardContent>
+//   </CardActionArea>
+// </Card>
+// </div>
+
+// <h2>Edit Menu</h2>
+// <img src={imglink} width="200" height="200" alt={imglink}  />
+// <h1>{name}<br />{notes}<br />{price}<br /></h1>
