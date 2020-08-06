c7da596ba209a8dd30358d22427e8b62e91bcd3f
Robert Balonek <robertbalonek@Roberts-MacBook.local>
2020-08-06 19:57:12 -0400

    Soleil Helped invent Delete Bill

--- src/components/Editor_Components/DeleteAllButton.js
+++ src/components/Editor_Components/DeleteAllButton.js
@@ -2,21 +2,8 @@ import React from "react";
 import axios from "axios";
 
 export default function DeleteAllButton(props) {
-
-  const deletePost = async () => {
-    await axios.delete(
-      "https://api.airtable.com/v0/app9S6k06MQoTSJbG/customerBill?filterByFormula=WHERE({price}='')",
-   // "https://api.airtable.com/v0/app9S6k06MQoTSJbG/customerBill?filterByFormula=NOT(%7Bprice%7D%3D0)",
-   // "https://api.airtable.com/v0/app9S6k06MQoTSJbG/customerBill?filterByFormula=price!=0",
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-        },
-      }
-    );
-    props.invokeFetch(!props.fetchEntries)
-   
-  };
+const {deletePost} = props
+  
   
   return (
     <div>
--- src/components/Guest_Components/BillPage.js
+++ src/components/Guest_Components/BillPage.js
@@ -3,9 +3,40 @@ import BillItems from "./BillItems"
 import DeleteAllButton from "../Editor_Components/DeleteAllButton"
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import { Link } from 'react-router-dom'
+import axios from "axios"
+
+const BASE_URL="https://api.airtable.com/v0/app9S6k06MQoTSJbG/customerBill/"
 
 export default function BillPage(props) {
-
+  ///Soleil to the rescue!
+ 
+  
+  const yeetAirtable = () => {
+    // get our bills
+    const billEntries = props.customerBill;
+    const post = setInterval(async () => {
+      // grabbing our current id
+      const bill = billEntries.pop();
+      // assess how many are left for our console log
+      const remaining = billEntries.length;
+      // this guy stops the interval in case there are no more entries
+      if (!bill) {
+        clearInterval(post);
+        return;
+      }
+      const { id} = bill
+      // make an axios request to the delete endpoint for entry with id ${id}
+      await axios.delete(`${BASE_URL}${id}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      // nice lil log
+      console.table(id);
+      console.log(`Above entry just deleted. ${remaining} entries left!`);
+    }, 250);
+  };
+  
   // console.log(props.customerBill)
   const total = props.customerBill.reduce((sum,curr) => sum+parseFloat(curr.fields.price),0)
   return (
@@ -18,13 +49,9 @@ export default function BillPage(props) {
       {props.customerBill.map((app) => <BillItems key={app.id} item={app} type="app" />)}
       <h1>Total</h1>
       <p>{total}</p>
-      <DeleteAllButton />
+      <DeleteAllButton
+        deletePost={yeetAirtable}
+        />
     </div>
   );
 }
-
-// {props.customerBill.flat().map((price) => <BillTotal key="billtotal" item={ price }/>)}
-
-//<BillItems key={props.bill.id} item={props.bill} type="bill"/> flatMap() 
-
-// <BillItems key={props.apps.id} item={props.apps} type="app"/>
